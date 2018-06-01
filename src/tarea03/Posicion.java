45e7817d0f6772b89d64630aed31fa1b98067a21
Francisco Jesús Latorre García <piti_fran@hotmail.com>
2018-06-01 13:49:29 +0200

    8. Constructor en la clase posicion.

--- src/tarea03/Posicion.java
+++ src/tarea03/Posicion.java
@@ -7,4 +7,18 @@ public class Posicion {
 
     private int fila;
     private char columna;
+
+    public Posicion(int fila, char columna) {
+        this.fila = fila;
+        this.columna = columna;
+
+        if ((fila >= 1 && fila <= 8) && (columna >= 'a' && columna <= 'h')) {
+            this.fila = fila;
+            this.columna = columna;
+        } else {
+            this.fila = 1;
+            this.columna = 'a';
+            System.out.printf("Error. No se ha insertado una posición válida.\n");
+        }
+    }
 }
