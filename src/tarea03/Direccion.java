0fe6acb9228bb63eda02403793761f14c7f5f134
Francisco Jesús Latorre García <piti_fran@hotmail.com>
2018-06-01 13:21:28 +0200

    6. Enumerado Direccion.

---
+++ src/tarea03/Direccion.java
@@ -0,0 +1,8 @@
+package tarea03;
+
+/**
+ * @author Francisco Jesus Latorre Garcia <franlatorregarcia@gmail.com>
+ */
+public enum Direccion {
+    NORTE, NORESTE, ESTE, SURESTE, SUR, SUROESTE, OESTE, NOROESTE
+}
