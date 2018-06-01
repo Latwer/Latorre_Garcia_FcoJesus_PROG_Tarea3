358b3e6a0f192b515ea35044618aaf120f4d0819
Francisco Jesús Latorre García <piti_fran@hotmail.com>
2018-06-01 14:04:29 +0200

    11. Comentarios JavaDoc.

--- src/tarea03/Posicion.java
+++ src/tarea03/Posicion.java
@@ -8,6 +8,10 @@ public class Posicion {
     private int fila;
     private char columna;
 
+    /**
+     * @param fila nos devolverá la fila desde el numero 1 al 8
+     * @param columna nos devolverá la columna de la letra 'a' a la 'h'
+     */
     public Posicion(int fila, char columna) {
         this.fila = fila;
         this.columna = columna;
@@ -22,14 +26,23 @@ public class Posicion {
         }
     }
 
+    /**
+     * @return nos devolverá la posición de la fila
+     */
     public int getFila() {
         return fila;
     }
 
+    /**
+     * @return nos devolverá la posición de la columna
+     */
     public char getColumna() {
         return columna;
     }
 
+    /**
+     * @param fila será comprobada para que esté entre el número '1' y el '8'
+     */
     public void setFila(int fila) {
         if (fila >= 1 && fila <= 8) {
             this.fila = fila;
@@ -39,6 +52,9 @@ public class Posicion {
         }
     }
 
+    /**
+     * @param columna será comprobado para que esté entre la letra 'a' y la letra 'h'
+     */
     public void setColumna(char columna) {
         if (columna >= 'a' && columna <= 'h') {
             this.columna = columna;
@@ -48,6 +64,9 @@ public class Posicion {
         }
     }
 
+    /**
+     * @return nos devolverá la posición de la columna y de la fila en forma de String
+     */
     @Override
     public String toString() {
         return String.format("Fila: %d, Columna: %s%n", fila, columna);
