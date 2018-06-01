3cb4c3f0bc4ae63b2a65d059d08c33228ae2f245
Francisco Jesús Latorre García <piti_fran@hotmail.com>
2018-06-01 14:15:36 +0200

    13. Constructor para la clase Rey que acepte como parámetros el color.

--- src/tarea03/Rey.java
+++ src/tarea03/Rey.java
@@ -8,4 +8,15 @@ public class Rey {
     private Color color;
     private Posicion posicion;
 
+    public Rey(Color color) {
+        this.color = color;
+        switch (color) {
+            case BLANCO:
+                posicion = new Posicion(1, 'e');
+                break;
+            case NEGRO:
+                posicion = new Posicion(8, 'e');
+                break;
+        }
+    }
 }
