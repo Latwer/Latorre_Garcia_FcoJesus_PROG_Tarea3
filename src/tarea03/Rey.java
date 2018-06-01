119dcc9499ca002367fa81d9e9ef2532f9fae78c
Francisco Jesús Latorre García <piti_fran@hotmail.com>
2018-06-01 14:09:33 +0200

    12. Clase Rey.

---
+++ src/tarea03/Rey.java
@@ -0,0 +1,11 @@
+package tarea03;
+
+/**
+ * @author Francisco Jesus Latorre Garcia <franlatorregarcia@gmail.com>
+ */
+public class Rey {
+
+    private Color color;
+    private Posicion posicion;
+
+}
