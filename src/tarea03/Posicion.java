b911ddf8501ec8b836716347bd61941395da3301
Francisco Jesús Latorre García <piti_fran@hotmail.com>
2018-06-01 13:23:26 +0200

    7. Clase Posicion y atributos fila y columna.

---
+++ src/tarea03/Posicion.java
@@ -0,0 +1,10 @@
+package tarea03;
+
+/**
+ * @author Francisco Jesus Latorre Garcia <franlatorregarcia@gmail.com>
+ */
+public class Posicion {
+
+    private int fila;
+    private char columna;
+}
